a69480d27670e86dd8c03970f97e08edc0a0bf85
Fengshan Yu <95190262+Fengshan-yu@users.noreply.github.com>
2022-02-02 21:16:37 -0500

    Update working_excel.py

--- working_excel.py
+++ working_excel.py
@@ -1,16 +1,16 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
-wb = load_workbook("MRI.xlsx")
+wb = load_workbook("apple.xlsx")
 ws = wb.active
 
-wb.create_sheet("BL")
-wb.create_sheet("W10")
+wb.create_sheet("usb")
+wb.create_sheet("usbc")
 
-ws_1 = wb["BL"]
-ws_2 = wb["W10"]
+ws_1 = wb["usb"]
+ws_2 = wb["usbc"]
 
-heading = ["subject", "time point", "DeltaV", "VV"]
+heading = ["date", "price", "adaptor", "current"]
 
 ws_1.append(heading)
 ws_2.append(heading)
@@ -18,15 +18,15 @@ ws_2.append(heading)
 for row in range(1, 101):
     for col in range(1, 6):
         char = get_column_letter(col)
-        if (ws[char + str(row)].value) == "BL":
+        if (ws[char + str(row)].value) == "usb":
             ws_1.append([(ws["A" + str(row)].value), (ws["B" + str(row)].value), (ws["C" + str(row)].value),
                          (ws["D" + str(row)].value)])
 
 for row in range(1, 101):
     for col in range(1, 6):
         char = get_column_letter(col)
-        if (ws[char + str(row)].value) == "W10":
+        if (ws[char + str(row)].value) == "usbc":
             ws_2.append([(ws["A" + str(row)].value), (ws["B" + str(row)].value), (ws["C" + str(row)].value),
                          (ws["D" + str(row)].value)])
 
-wb.save("MRI.xlsx")
+wb.save("apple.xlsx")
